274236bf3e922e2dcfafde482fb17afe2e2bc3f1
Alive-coder <1850290043@qq.com>
2021-09-16 22:54:54 +0800

    9.16 commit

--- src/main.js
+++ src/main.js
@@ -5,6 +5,23 @@ Vue.config.productionTip = false
 
 App.mpType = 'app'
 
+Vue.filter('formatCount', value => {
+  if(value >= 10000 && value <= 100000000){
+    value /= 10000
+    return value.toFixed(1) + '万'
+  }else if(value > 100000000){
+    value /= 100000000
+    return value.toFixed(1) + '亿'
+  }else{
+    return value
+  }
+})
+
+Vue.filter('formatTime', value => {
+  let date = new Date(value)
+  return date.getFullYear() + '年' + date.getMonth() + '月' + date.getDate() + '日'
+})
+
 const app = new Vue({
   ...App
 })
